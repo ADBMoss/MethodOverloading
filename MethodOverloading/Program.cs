ec9f27dc9e4a94bc5e576db0d5fa4b809fae9566
DarriusB <darrius.bradford@gmail.com>
2022-04-28 08:56:09 -0700

    Method Overloading Practice

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{Program.Add(5, 5)}");
+            Console.WriteLine($"{Program.Add(5.6, 3.9)}");
+            Console.WriteLine($"{Program.Add(1, 1, true)}");
+        }
+
+
+        public static int Add(int x, int y)
+        {
+            int result = int.Parse(Convert.ToString(x + y));
+            return result;   
+        }
+
+        public static double Add(double x, double y)
+        {
+            double result = double.Parse($"{x + y}");
+            return result;
+        }
+
+        public static string Add(int x, int y, bool z)
+        {
+            if (z == true && x + y == 1)
+            {
+                string result = string.Concat($"{x + y} dollar");
+                return result;
+                //($"{x + y} + dollars");
+            }
+            else if (z == true)
+            {
+                string result = string.Concat($"{x + y} dollars");
+                return result;
+                //($"{x + y} + dollar");
+            }
+            else
+            {
+                return ($"{x + y}");
+            }
+        }
+    }
+}
